70ca32b696942218d5987b14e52e3e1ece2adc49
medmahmouduvs <mohamedmahmoud.elmamoun@uvs.edu.sn>
2021-10-17 08:41:32 +0000

    Steps 01 et 02

---
+++ soap-course-managment/soap-course-managment/src/test/java/com/ws/soap/webservices/soapcoursemanagment/SoapCourseManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.soap.webservices.soapcoursemanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
